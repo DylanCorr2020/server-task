d7797639bf323c98e818cfb60172633e534d635c
Dylan Corr <dylancorr.g@gmail.com>
2025-03-08 12:50:21 +0000

    Created database Todo-Tasks

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,16 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create DynamoDB table
+    // primary key id
+    // table will be deleted along with all its data
+    // pay per request billing mode
+    const todoTable = new dynamodb.Table(this, 'TodoTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      tableName: 'Todo-Tasks',
+      removalPolicy: cdk.RemovalPolicy.DESTROY, 
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
+
+
   }
 }
